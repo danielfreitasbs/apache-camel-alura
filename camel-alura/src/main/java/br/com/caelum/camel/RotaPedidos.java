e24876e122f6c27253ab432f12d45861983ec166
Daniel Freitas <daniel.italofreitas@gmail.com>
2021-08-31 14:26:51 -0300

    Finalização do curso sem envio apartado de msg

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,7 +1,9 @@
 package br.com.caelum.camel;
 
+import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -11,23 +13,55 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
+		
+		context.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61616"));
 		context.addRoutes(new RouteBuilder() {
 
 			@Override
 			public void configure() throws Exception {
-				from("file:pedidos?delay=5s&noop=true")
-					.setProperty("pedidoId", xpath("/pedido/id/text()"))
-					.setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()"))
-					.split()
-						.xpath("/pedido/itens/item").log("${body}")
-				.filter()
-					.xpath("/item/formato[text()='EBOOK']").log("${id} - ${body}")
-				.setProperty("ebookId", xpath("/item/livro/codigo/text()"))
-				.marshal()
-					.xmljson().log("${body}")
-				.setHeader(Exchange.HTTP_METHOD, HttpMethods.GET)
-				.setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=&{property.clienteId}"))
+				
+				errorHandler(deadLetterChannel("activemq:queue:pedidos.DLQ").logExhaustedMessageHistory(true)
+						.maximumRedeliveries(3)
+							.redeliverDelay(2000)
+						.onRedelivery(new Processor() {
+
+							@Override
+							public void process(Exchange exchange) throws Exception {
+								System.out.println("Redelivery");
+							}
+
+						}));
+				
+				from("activemq:queue:pedidos")
+				.routeId("rota-pedidos")
+				.to("validator:pedido.xsd")
+				.multicast()
+					.to("direct:soap")
+					.to("direct:http");
+				
+				from("direct:http")
+					.routeId("rota-http")
+						.log("${body}")
+						.setProperty("pedidoId", xpath("/pedido/id/text()"))
+						.setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()"))
+						.split()
+							.xpath("/pedido/itens/item").log("${body}")
+						.filter()
+							.xpath("/item/formato[text()='EBOOK']").log("${id} - ${body}")
+						.setProperty("ebookId", xpath("/item/livro/codigo/text()"))
+						.marshal()
+							.xmljson().log("${body}")
+						.setHeader(Exchange.HTTP_METHOD, HttpMethods.GET)
+						.setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=&{property.clienteId}"))
 				.to("http4://localhost:8080/webservices/ebook/item");
+				
+				from("direct:soap")
+					.routeId("rota-soap")
+					.setBody(constant("<envelope>teste</envelope>"))
+					.to("xslt:pedido-para-soap.xslt")
+					.log("${body}")
+					.setHeader(Exchange.CONTENT_TYPE, constant("text/xml"))
+				.to("http4://localhost:8080/webservices/financeiro");
 			}
 
 		});
